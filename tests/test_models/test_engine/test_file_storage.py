1f239053881827b71d821f4eb6e18a3a304b017b
yonessgt <youness.merzak123@gmail.com>
2023-08-12 23:55:52 +0000

    unitest for file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,2 +1,69 @@
 #!/usr/bin/python3
 """  test for file_storage module"""
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import models
+import os
+from models.engine.file_storage import FileStorage
+
+
+class TestFilestorage(unittest.TestCase):
+    """CLass created to test all the cases of FireStorage class"""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "temp.json")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("temp.json", "file.json")
+        except IOError:
+            pass
+
+    def test_all(self):
+        """testing the all method"""
+
+        store = FileStorage()
+        self.assertEqual(type(store.all()), dict)
+        self.assertIs(store.all(), store._FileStorage__objects)
+
+    def test_all_with_arg(self):
+        """testing the all method with  arguments"""
+
+        store1 = FileStorage()
+        with self.assertRaises(TypeError):
+            store1.all(25)
+
+    def test_new(self):
+        """testing the new method"""
+
+        base2 = BaseModel()
+        base2.id = "897"
+        store2 = FileStorage()
+        store2.new(base2)
+        self.assertIn("BaseModel.897", store2.all())
+
+    def test_Reload(self):
+        """testing the reload method"""
+
+        base3 = BaseModel()
+        base3.id = "963"
+        storage = FileStorage()
+        storage.new(base3)
+        base3.save()
+        storage.reload()
+        self.assertIn("BaseModel.963", storage.all())
+
+
+if __name__ == "__main__":
+    unittest.main()
