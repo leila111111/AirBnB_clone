6432eff08796acd27354c451aea16e5ef684a231
leila111111 <leila_merzak@outlook.fr>
2023-08-12 18:00:10 +0000

    test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,7 +10,43 @@ class Testbasemodel(unittest.TestCase):
     """class created to do test cases
     for base_model module"""
 
-    pass
+    def test_attr(self):
+        """test attributes"""
+
+        model = BaseModel()
+        model.name = "model"
+        model.id = "115"
+        self.assertTrue(hasattr(model, "name"))
+        self.assertTrue(hasattr(model, "id"))
+        self.assertEqual(str, type(model.id))
+        self.assertEqual(str, type(model.name))
+        self.assertTrue(datetime, type(model.created_at))
+        self.assertTrue(datetime, type(model.updated_at))
+        self.assertEqual(len(model.__dict__), 4)
+
+    def test_represent(self):
+        """test cases for the object representations"""
+
+        model1 = BaseModel()
+        model1.id = "116"
+        date = datetime.now()
+        model1.created_at = model1.updated_at = date
+        dateRep = repr(date)
+        string = str(model1)
+        self.assertIn("[BaseModel] (116)", string)
+        self.assertIn("'created_at': " + dateRep, string)
+        self.assertIn("'updated_at': " + dateRep, string)
+        self.assertIn("'id': '116'", string)
+
+    def test_toDict(self):
+        """test cases for the to_dict method"""
+        pass
+
+    def test_instant(self):
+        """testing instantiation"""
+
+        model2 = BaseModel()
+        self.assertIsInstance(model2, BaseModel)
 
 
 if __name__ == "__main__":
