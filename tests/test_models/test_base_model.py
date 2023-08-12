4abebad1ab1b218d4b152fadfa5ac4c6a29e2c0e
yonessgt <youness.merzak123@gmail.com>
2023-08-12 16:27:19 +0000

    unitest base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 """  test for base_model module"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import models
+
+
+class Testbasemodel(unittest.TestCase):
+    """class created to do test cases
+    for base_model module"""
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
